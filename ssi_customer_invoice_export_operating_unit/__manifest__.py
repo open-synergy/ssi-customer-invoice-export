82ac655850be033f8df05063ec013e3eb0864d1d
Andhitia Rama <andhitia.r@gmail.com>
2026-07-06 21:54:50 +0700

    [ADD] ssi_customer_invoice_export_operating_unit
    Modul glue Operating Unit (OU) untuk ssi_customer_invoice_export.
    Menambahkan field operating_unit_id pada dokumen Customer Invoice
    Export, dan meng-override kode (bukan sekadar ir.rule) agar tombol
    Populate hanya menarik invoice pelanggan dan journal yang diizinkan
    sesuai OU yang dipilih pada dokumen. Bila OU kosong, perilaku
    Populate tetap seperti modul dasar (tanpa batasan).

---
+++ ssi_customer_invoice_export_operating_unit/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Customer Invoice Export + Operating Unit",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "contributors": [
+        "Andhitia Rama <andhitia.r@gmail.com>",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_customer_invoice_export",
+        "ssi_operating_unit_mixin",
+        "ssi_financial_accounting_operating_unit",
+    ],
+    "data": [
+        "security/res_group/customer_invoice_export.xml",
+        "security/ir_rule/customer_invoice_export.xml",
+        "view/customer_invoice_export.xml",
+    ],
+}
---
+++ ssi_customer_invoice_export_operating_unit/models/customer_invoice_export.py
@@ -0,0 +1,29 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class CustomerInvoiceExport(models.Model):  # pylint: disable=too-few-public-methods
+    _name = "customer_invoice_export"
+    _inherit = [
+        "customer_invoice_export",
+        "mixin.single_operating_unit",
+    ]
+
+    def _prepare_invoice_domain(self):
+        domain = super()._prepare_invoice_domain()
+        if self.operating_unit_id:
+            domain.append(("operating_unit_id", "=", self.operating_unit_id.id))
+        return domain
+
+    @api.depends("type_id", "operating_unit_id")
+    def _compute_allowed_journal_ids(self):
+        super()._compute_allowed_journal_ids()
+        for record in self:
+            if record.operating_unit_id and record.allowed_journal_ids:
+                record.allowed_journal_ids = record.allowed_journal_ids.filtered(
+                    lambda journal, record=record: record.operating_unit_id
+                    in journal.operating_unit_ids
+                )
